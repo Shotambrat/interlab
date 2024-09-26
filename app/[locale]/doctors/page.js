148ea14a4294c08e7b7378419e2a36ade4cb9156
Abu <dudumikah@gmail.com>
2024-09-26 19:01:27 +0500

    feat: update: hhh

--- app/[locale]/doctors/page.js
+++ app/[locale]/doctors/page.js
@@ -1,18 +1,16 @@
 // app/[locale]/doctors.page.js
-import DoctorsBanner from '@/app/[locale]/_components/doctors/DoctorsBanner';
-import Filter from '@/app/[locale]/_components/doctors/Filter';
+import DoctorsBanner from "@/app/[locale]/_components/doctors/DoctorsBanner";
+import Filter from "@/app/[locale]/_components/doctors/Filter";
 import { client } from "@/sanity/lib/client";
 
 export default async function HomePage({ params }) {
   // Fetching doctor data from Sanity
-  const doctors = await client.fetch(`*[_type == "doctor"]{
+  const doctors = await client.fetch(`*[_type == "doctor"] {
     name,
     slug,
     photo,
     position
   }`);
-
-  console.log("DOCTORS", doctors)
 
   return (
     <div>
