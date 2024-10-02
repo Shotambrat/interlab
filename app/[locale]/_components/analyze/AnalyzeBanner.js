e7c725719b97fa111fe8478b4a8ab5e13f7757a1
Abu <dudumikah@gmail.com>
2024-10-02 10:55:35 +0500

    feat: update: analyze Banner

--- app/[locale]/_components/analyze/AnalyzeBanner.js
+++ app/[locale]/_components/analyze/AnalyzeBanner.js
@@ -5,7 +5,7 @@ import { useTranslations } from "next-intl";
 function AnalyzeBanner() {
   const t = useTranslations();
   return (
-    <div className="bg-sky-100 w-full px-4 lg:px-0 py-10 h-full max-h-[400px]">
+    <div className="bg-sky-100 w-full px-4 lg:px-0 py-10 h-full lg:max-h-[400px]">
       <div className="max-w-[1440px] w-full mx-auto flex flex-col mdl:flex-row items-center h-full overflow-hidden">
         {/* Left side - title and description */}
         <div className="flex flex-col w-full mdl:max-w-[800px] text-left justify-center">
@@ -16,13 +16,13 @@ function AnalyzeBanner() {
         </div>
         
         {/* Right side - image */}
-        <div className="mdl:w-1/2 flex justify-end">
+        <div className="mdl:w-3/4 w-full flex justify-end max-mdl:justify-center">
           <Image
             quality={100}
             src={analyzeBanner}
             width={1000}
             height={1000}
-            className="w-full max-w-[300px] mr-12 object-contain"
+            className="w-full max-w-[300px] mdl:mr-12 object-contain"
             alt="Analyze Banner"
           />
         </div>
