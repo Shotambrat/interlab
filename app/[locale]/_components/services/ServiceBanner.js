36b935112c4d19b64b2be84311e41d671895238e
Faizullin Abdukhakim <dudumikah@gmail.com>
2024-10-17 19:01:04 +0500

    feat: update: services babner

--- app/[locale]/_components/services/ServiceBanner.js
+++ app/[locale]/_components/services/ServiceBanner.js
@@ -6,14 +6,14 @@ import { useTranslations } from "next-intl";
 export default function ServiceBanner({ locale }) {
   const t = useTranslations();
   return (
-    <div className="h-screen-70 mdx:h-screen-90 bg-serviceBg relative pt-10 xl:pb-[100px] pb-[120px] px-2">
-      <div className="mx-auto w-full h-full max-w-[1440px] flex max-mdx:flex-col-reverse justify-between items-center">
+    <div className="mdx:h-screen-90 bg-serviceBg relative pt-10 xl:pb-[100px] pb-10 px-2">
+      <div className="mx-auto w-full h-full max-w-[1440px] flex max-mdx:flex-col-reverse max-mdx:gap-10 justify-between items-center">
         <div className="flex flex-col w-full xl:w-2/5">
           <h1 className="xl:text-6xl slg:text-4xl text-3xl font-bold text-center xl:text-left">
             {t("Services.title")}{" "}
             <span className="text-red-400">{t("Services.subtitle")}</span>
           </h1>
-          <p className="text-neutral-600 leading-6 slg:leading-4 text-lg xl:leading-6 text-center xl:text-left">
+          <p className="text-neutral-600 max-mdx:hidden leading-6 slg:leading-4 text-lg xl:leading-6 text-center xl:text-left">
             {t("Services.description")}
           </p>
         </div>
