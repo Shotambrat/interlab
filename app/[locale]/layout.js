32186321be2b386d7a8d6ab904503186e4d9d09c
Faizullin Abdukhakim <dudumikah@gmail.com>
2024-10-18 14:01:44 +0500

    feat: update: Added Yandex Metrics

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -5,6 +5,7 @@ import { getMessages } from 'next-intl/server';
 import Header from "@/app/[locale]/_components/Header";
 import Footer from "@/app/[locale]/_components/Footer";
 import CallToAction from './_components/Modals/CallToAction';
+import Script from 'next/script';
 
 export const metadata = {
   title: {
@@ -16,8 +17,7 @@ export const metadata = {
     icon: '/favicon.ico'
   },
   manifest: '/manifest.json'
-}
-
+};
 
 export default async function LocaleLayout({ children, params }) {
   const { locale } = params;
@@ -29,6 +29,39 @@ export default async function LocaleLayout({ children, params }) {
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Header locale={locale} />
           <CallToAction locale={locale} />
+          
+          {/* Яндекс.Метрика */}
+          <Script
+            id="yandex-metrika"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(98667284, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true,
+                  ecommerce:"dataLayer"
+              });
+              `,
+            }}
+          />
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/98667284"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+              />
+            </div>
+          </noscript>
+
           <main>{children}</main>
           <Footer locale={locale} />
         </NextIntlClientProvider>
